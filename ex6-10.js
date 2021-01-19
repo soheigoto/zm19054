64fa9b177d134a81ef060bca4e45942444456b52
soheigoto <zm19054@josai.ac.jp>
2021-01-19 01:37:04 +0000

    1/19

---
+++ ex6-10.js
@@ -0,0 +1,8 @@
+function imgchange01() {
+    document.getElementById("logo01").src=
+     "./image/onepiece06_chopperⅡ.png";
+    }
+    function imgchange02() {
+    document.getElementById("logo01").src=
+     "./image/onepiece06_chopperⅠ.png";
+    }
